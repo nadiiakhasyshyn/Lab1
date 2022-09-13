91690b4907b1bc4ed2055f0c382d80f7462ac885
nadiiakhasyshyn <nadiia.khasyshyn.pm.2020@lpnu.ua>
2022-09-13 17:27:58 +0300

    Lab1

---
+++ AnalizerClassLibraryTests/AnalaizerClassTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AnalaizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+        
+        
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "ValidTest", DataAccessMethod.Sequential)]
+        public void IsOperator_WhenIncomingDataIsValid_ReturnsTrue()
+        {
+            //Arrange
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+            string initial = Convert.ToString(TestContext.DataRow["incomingData"]);
+            //Actual
+            bool actualResult = AnalaizerClass.IsOperator(initial);
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "InValidTest", DataAccessMethod.Sequential)]
+        public void IsOperator_WhenIncomingDataIsValid_ReturnsFalse()
+        {
+            //Arrange
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+            string initial = Convert.ToString(TestContext.DataRow["incomingData"]);
+            //Actual
+            bool actualResult = AnalaizerClass.IsOperator(initial);
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "ValidTest2", DataAccessMethod.Sequential)]
+        public void IsDelimeter_WhenIncomingDataIsValid_ReturnsTrue()
+        {
+            //Arrange
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+            char initial = Convert.ToChar(TestContext.DataRow["incomingData"]);
+            //Actual
+            bool actualResult = AnalaizerClass.IsDelimeter(initial);
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "InValidTest2", DataAccessMethod.Sequential)]
+        public void IsDelimeter_WhenIncomingDataIsValid_ReturnsFalse()
+        {
+            //Arrange
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+            char initial = Convert.ToChar(TestContext.DataRow["incomingData"]);
+            //Actual
+            bool actualResult = AnalaizerClass.IsDelimeter(initial);
+            //Assert
+            Assert.AreEqual(expected, actualResult);
+        }
+
+    }
+}
